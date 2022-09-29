8bcafc7dab0be73ae6230b56fbe3b0ee90297acd
Albin Lindqvist <albin.lindqvist@elev.ga.ntig.se>
2022-09-29 14:59:05 +0200

    intital combat

---
+++ detch_figt/Program.cs
@@ -0,0 +1,35 @@
+﻿
+string fightu = "kör";
+
+
+while (fightu == "kör")
+{
+    
+}
+
+
+
+
+
+
+Console.ReadLine();
+
+// Random general = new Random();
+
+// int hitRoll = general.Next(100);
+
+// Console.WriteLine(hitRoll);
+
+// if (hitRoll > 95)
+// {
+//     Console.WriteLine("crit");
+// }
+// else if (hitRoll > 40)
+// {
+//     Console.WriteLine("hit");
+// }
+// else
+// {
+//     Console.WriteLine("miss");
+// }
+// Console.ReadLine();
