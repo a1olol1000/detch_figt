a48d307828cfcfaadeded3f49a63f2ab89e1a406
Albin Lindqvist <albin.lindqvist@elev.ga.ntig.se>
2022-10-07 12:20:24 +0200

    secund combat

--- detch_figt/Program.cs
+++ detch_figt/Program.cs
@@ -1,14 +1,66 @@
 ﻿
 string fightu = "kör";
+Random general = new Random();
+int hitpoint = general.Next(100);
+int strength = general.Next(20);
+int chans = general.Next(100);
+int heal = general.Next(20);
+int chans_chans = general.Next(100);
+
+
+bool live_1 = true;
+bool live_2 = true;
 
 
 while (fightu == "kör")
 {
-    
+    Console.WriteLine("fighting game press any key to start");
+    Console.ReadLine();
+    chans = general.Next(100);
+    if (chans < 50)
+    {
+        chans = 50;
+    }
+    else if (chans > 90)
+    {
+        chans = 90;
+    }
+    hitpoint = general.Next(100);
+    strength = general.Next(20);
+    if (hitpoint < 50)
+    {
+        hitpoint = 50;
+    }
+    if (strength < 5)
+    {
+        strength = 5;
+    }
+    while (live_1 && live_2)
+    {
+        chans_chans = general.Next(100); 
+        
+        Console.WriteLine($"hp({hitpoint}) str({strength}) hit Chanse({chans}) hit({sucsses})");
+        Console.ReadLine();
+        
+    }
+
 }
 
 
-
+static bool sucsses(int chans_chans, int chans)
+{
+    Random stat = new Random();
+    chans_chans = stat.Next(100);
+    if (chans_chans < chans)
+    {
+        result = true;
+    }
+    else if (chans_chans > chans)
+    {
+        result = false;
+    }
+    return result;
+}
 
 
 
